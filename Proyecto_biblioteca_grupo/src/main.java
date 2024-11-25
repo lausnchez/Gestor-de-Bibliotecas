b668db886916b9e6001a07f8d44ecb2f12d4f5d8
lausnchez <lausnchezmartn@gmail.com>
2024-11-25 12:48:26 +0100

    Intento de subida final

--- Proyecto_biblioteca_grupo/src/main.java
+++ Proyecto_biblioteca_grupo/src/main.java
@@ -17,7 +17,6 @@ import vista.usuarioLoginView;
  */
 public class main {
     public static void main(String[] args) {  
-        //new ControladorMostrarBibliotecas();
         // Crear la vista de login
         usuarioLoginView loginView = new usuarioLoginView();
         UsuarioController controller = new UsuarioController(loginView);
